9dec9e4f09742832051aef22f7de0bc731f6fd56
zelvaiz <zelvaimam15@gmail.com>
2024-06-14 18:23:43 +0700

    update macro age_category

--- include/dbt/project/macros/age_category.sql
+++ include/dbt/project/macros/age_category.sql
@@ -1,9 +1,8 @@
 {% macro age_category(column_name) %}
     case
-        when {{column_name}} >= 30 then 'Adult'
+        when {{column_name}} >= 40 then 'Old Adults'
+        when {{column_name}} >= 30 then 'Middle-aged Adults'
         when {{column_name}} >= 20 then 'Young Adult'
         when {{column_name}} >= 13 then 'Teen'
-        when {{column_name}} >= 5 then 'Kid'
-        when {{column_name}} >= 0 then 'Baby'
     end
 {% endmacro %}
